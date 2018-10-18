ab5d86312d9c91de35604a09d9f6c87673f8430a
maxivan41 <maxivan41@gmail.com>
2018-10-18 11:28:22 +0300

    new

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import {HashRouter, Route, Switch } from "react-router-dom";
 import Categories from "./Categories";
 import Basket from "./CheckOut";
 import ProductContainer from "./Product";
@@ -7,12 +7,14 @@ import ProductContainer from "./Product";
 class AppRouter extends Component {
   public render() {
     return (
-      <Switch>
-        <Route exact path="/" component={Categories} />
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={Categories} />
 
-        <Route path="/products/:id" component={ProductContainer} />
-        <Route exact path="/check-out" component={Basket} />
-      </Switch>
+          <Route path="/products/:id" component={ProductContainer} />
+          <Route exact path="/check-out" component={Basket} />
+        </Switch>
+      </HashRouter>
     );
   }
 }
